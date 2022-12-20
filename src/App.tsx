fcec9b8140839a8927dc651c02ebf9df958340f4
Hyuksang-Oh <iuesver@gmail.com>
2022-12-20 19:06:55 +0900

    add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { CreatePage } from './pages/CreatePage';
 import { MainPage } from './pages/MainPage';
+import { NotFoundPage } from './pages/NotFoundPage';
 import { ProductPage } from './pages/ProductPage';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="/:id" element={<ProductPage />} />
+        <Route path="/*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import tw from 'tailwind-styled-components';
+
+const HeroDiv = tw.div`
+hero min-h-screen
+`;
+
+const HeroContent = tw.div`
+hero-content text-center
+`;
+
+export const NotFoundPage = () => {
+  return (
+    <HeroDiv>
+      <HeroContent>
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold py-4">404 ERROR!</h1>
+          <p className="text-2xl font bold py-2">페이지를 찾을 수 없습니다.</p>
+          <p className="pb-2">
+            페이지가 사라졌거나, 잘못된 경로를 입력하셨습니다.
+            <br />
+            올바른 URL을 입력하였는지 확인해주세요.
+          </p>
+          <Link to="/">
+            <button className="btn btn-primary text-white">
+              홈으로 돌아가기
+            </button>
+          </Link>
+        </div>
+      </HeroContent>
+    </HeroDiv>
+  );
+};
